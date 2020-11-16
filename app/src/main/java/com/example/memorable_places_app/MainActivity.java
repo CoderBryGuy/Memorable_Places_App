a0a12d966cc2c38d1af573a70bf338715211b469
Bryan <bryan.dov.bergman@gmail.com>
2020-11-16 23:32:00 +0200

    Initial commit

---
+++ app/src/main/java/com/example/memorable_places_app/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.memorable_places_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView myListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myListView = (ListView) findViewById(R.id.myListView);
+
+        ArrayList<String> places = new ArrayList<>();
+
+        places.add("Add a new places...");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, places);
+
+        myListView.setAdapter(arrayAdapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                intent.putExtra("placeNumber", i);
+
+                startActivity(intent);
+            }
+        });
+    }
+}
